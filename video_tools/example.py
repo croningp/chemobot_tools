986d55431c2727864441343f7f0d8ee89b9ac225
jgrizou <jonathan.grizou@glasgow.ac.uk>
2016-07-08 12:00:57 +0100

    removed duplicated code from kevin

--- video_tools/example.py
+++
@@ -1,8 +0,0 @@
-#sys.path.append('./platform')
-import video_tools
-video_recorder = video_tools.VideoRecorder(0)
-
-
-video_recorder.record_to_file(video_filepath,  recording_time)
-
-video_recorder.wait_until_idle()
